29aeae330a1535cf3c48441022993fd98669a297
Maxim <mihalenko.maxim21@gmail.com>
2023-04-07 02:59:00 +0300

    add request to jsonplaceholder

--- index.js
+++ index.js
@@ -115,6 +115,22 @@ const signUp = (event) => {
   event.preventDefault();
   const errors = validateSignUpForm(form);
   printSignUpErrors(errors);
+
+  if (Object.values(errors).every((error) => !error)) {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then(console.log);
+  }
 };
 
 addFormSubmition("sign-up-form", signUp);
