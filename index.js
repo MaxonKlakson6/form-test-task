2eb3874ece960dc76aedc49ea65743b7b562386e
Maxim <mihalenko.maxim21@gmail.com>
2023-04-07 02:41:55 +0300

    add errors printing

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const form = {
+  name: "",
+  surname: "",
+  birthDate: "",
+  email: "",
+  password: "",
+  confirm: "",
+};
+
+const emailRegExp =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordRegExp = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%])[A-Za-z\d!@#$%]{8,}$/;
+
 const useForm = (form = {}) => {
-  Object.entries(form).forEach(([name, value]) => {
+  Object.entries(form).forEach(([name]) => {
     const input = document.querySelector(`input[name=${name}]`);
 
     input.addEventListener("input", (event) => {
@@ -29,15 +42,79 @@ const useVisibilityInputs = (buttonClassName) => {
   });
 };
 
-
-const form = {
-  name: "",
-  surname: "",
-  birthDate: "",
-  email: "",
-  password: "",
-  confirm: "",
-};
-
 useVisibilityInputs("visibility-btn");
 useForm(form);
+
+const addFormSubmition = (formId, submitCallback) => {
+  const form = document.querySelector(`#${formId}`);
+
+  form.addEventListener("submit", submitCallback);
+};
+
+const validateSignUpForm = (form) => {
+  const errors = {};
+  const today = new Date();
+
+  if (!form.name) {
+    errors.name = "Name is a required field";
+  } else if (form.name.length < 2) {
+    errors.name = "Name too short";
+  } else if (form.name.length > 25) {
+    errors.name = "Name too long";
+  }
+
+  if (!form.surname) {
+    errors.surname = "Surname is a required field";
+  } else if (form.surname.length < 2) {
+    errors.surname = "Surname too short";
+  } else if (form.surname.length > 25) {
+    errors.surname = "Surname too long";
+  }
+
+  if (!form.birthDate) {
+    errors.birthDate = "Birth date is a required field";
+  } else if (new Date(form.birthDate) > today) {
+    errors.birthDate = "Today is a max available date";
+  }
+
+  if (!form.email.toLowerCase().match(emailRegExp)) {
+    errors.email = "Email must be an email";
+  }
+
+  if (!form.password.match(passwordRegExp)) {
+    errors.password = "Wrong password";
+  }
+
+  if (form.confirm !== form.password) {
+    errors.confirm = "Passwords aren't equal";
+  }
+
+  return errors;
+};
+
+const createErrorPrinter = () => {
+  const some = Object.entries(form).map(([name]) => ({
+    name,
+    node: (noticeBlock = document.querySelector(`#notice-${name}`)),
+  }));
+
+  return (errors) => {
+    some.forEach(({ name, node }) => {
+      if (!errors[name]) {
+        node.innerHTML = "";
+      } else {
+        node.innerHTML = errors[name];
+      }
+    });
+  };
+};
+
+const printSignUpErrors = createErrorPrinter();
+
+const signUp = (event) => {
+  event.preventDefault();
+  const errors = validateSignUpForm(form);
+  printSignUpErrors(errors);
+};
+
+addFormSubmition("sign-up-form", signUp);
