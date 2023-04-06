5c72916dc22cf61ad4503c11423f44f7b1b5d556
Maxim <mihalenko.maxim21@gmail.com>
2023-04-06 19:40:38 +0300

    Move scripts to index.js file because of cors error

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-import { useForm } from "./scripts/useForm.js";
-import { useVisibilityInputs } from "./scripts/useVisibilityInputs.js";
+const useForm = (form = {}) => {
+  Object.entries(form).forEach(([name, value]) => {
+    const input = document.querySelector(`input[name=${name}]`);
+
+    input.addEventListener("input", (event) => {
+      const { name, value } = event.target;
+      form[name] = value;
+    });
+  });
+};
+
+const useVisibilityInputs = (buttonClassName) => {
+  const visibilityButtonList = document.querySelectorAll(`.${buttonClassName}`);
+
+  Array.from(visibilityButtonList).forEach((button) => {
+    const inputId = button.attributes.for.value;
+    const input = document.querySelector(`#${inputId}`);
+
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      event.target.classList.toggle(`${buttonClassName}-on`);
+
+      if (input.type === "password") {
+        input.type = "text";
+      } else {
+        input.type = "password";
+      }
+    });
+  });
+};
+
 
 const form = {
   name: "",
